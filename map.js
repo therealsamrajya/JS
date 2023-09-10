2c653bc5b5c31f720cd2703b4a97062c9f8d059d
therealsamrajya <samrajyadai@gmail.com>
2023-09-10 09:03:23 +0545

    Upto DOM BOM

---
+++ map.js
@@ -0,0 +1,19 @@
+// const array1 = [1,4,9,15,20]
+// let newArr = array1.map((currElm,index,arr)=> 
+// {
+//   return `Index no = ${index} and the value is ${currElm} belong to ${arr}`
+// })
+// console.log(newArr);
+
+// square root of a number
+// const array  = [16,25,36,49]
+// let newArr = array.map((curElm)=> Math.sqrt(curElm))
+// console.log (newArr);
+// multiply by 2 & return elm greater than 10
+// reduce function:returns only one value used to add sub at once
+// let array = [2,3,4,6,8]
+// let currElm = array.map((currElm) =>currElm *2).filter((currElm)=> currElm > 10).reduce((accumuator, currElm)=>
+// {
+//   return accumuator+= currElm;
+// });
+// console.log(currElm);
